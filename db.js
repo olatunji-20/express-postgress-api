af33ab7abe26b6a4d81047865a5ed49824e457f8
falana sheriff <falanasheriffdeen@gmail.com>
2023-03-11 12:19:57 +0100

    prep for docker-compose

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "postgres",
     host: "localhost",
-    database: "students",
+    database: "cfc",
     password: "ola",
     port: 5432
 });
