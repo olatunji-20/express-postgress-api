5d5ea130d0ef8208fb4261277920b0ceec0bec8a
falana sheriff <falanasheriffdeen@gmail.com>
2023-03-23 09:49:28 +0100

    work work work

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    host: "db",
+    host: "db", //host changed to db(instead of localhost) because of docker service(db)
     database: "cfc",
     password: "ola",
     port: 5432
 });
-// host changed to db(instead of localhost) because of docker services
+
 module.exports = pool;
